30031d544d507c76b07c9621c7089a146e4f3e3f
mohabzalat <mohabzalat20@gmail.com>
2024-09-04 21:50:06 +0300

    adding livewire to make alpine works

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
+@livewireStyles
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -15,3 +16,5 @@
         </div>
     </div>
 </x-app-layout>
+@livewireScripts
+
