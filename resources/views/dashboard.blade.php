67b653ea9633a0812cfb2fd5bafe32d48867363d
mohabzalat <mohabzalat20@gmail.com>
2024-09-05 03:15:31 +0300

    working on user dashboard (profile-settings)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,90 @@
-@livewireStyles
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+        <title>Laravel</title>
+
+        @vite(['resources/css/app.css','resources/js/app.js'])
+    </head>
+    <body class="font-sans bg-gray-100 antialiased dark:bg-black dark:text-white/50 max-w-[1500px] mx-auto">
+        {{-- dashboard --}}
+        @include('layouts.custom-navigation')
+        <div class="container mx-auto">
+            <div class="flex flex-wrap items-center justify-between p-10">
+                <div>
+                    <p class="p-0 m-0 text-gray-700 text-5xl font-bold">{{Auth::user()->name}}</p>
+                    <p class="p-0 m-2 text-gray-500 text-base font-semibold">{{Auth::user()->email}}</p>
+                </div>
+                <div class="mt-2 lg:mt-0">
+                    <form action="{{route('logout')}}" method="POST">
+                        @csrf
+                        <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-full px-5 py-4 text-center">Logout</button>
+                    </form>
+                </div>
+            </div>
+            <div class="bg-slate-200 rounded-lg">
+                <ul class="flex justify-center font-bold">
+                    <li class="text-xl hover:bg-slate-300 p-5">Edit Profile</li>
+                    <li class="text-xl hover:bg-slate-300 p-5">Tickets</li>
+                    <li class="text-xl hover:bg-slate-300 p-5">Wallets</li>
+                </ul>
+            </div>
+            {{-- profile --}}
+            <div class="p-10">
+                {{-- profile information --}}
+                <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <form class="space-y-6" action="#">
+                        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Profile Information</h5>
+                        <p class="text-sm text-gray-500">
+                            Update your account's profile information and email address.
+                        </p>
+                        <div>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                            <input type="text" name="text" id="name" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name" />
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input type="email" name="email" id="email" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" />
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                    </form>
+                </div>
+                {{-- password --}}
+                <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <form class="space-y-6" action="#">
+                        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Update Password</h5>
+                        <p class="text-sm text-gray-500">
+                            Ensure your account is using a long, random password to stay secure.
+                        </p>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
+                            <input type="password" name="passowrd" id="password" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name" />
+                        </div>
+                        <div>
+                            <label for="newpassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                            <input type="password" name="newpassword" id="newpassword" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name" />
+                        </div>
+                        <div>
+                            <label for="confirmpassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                            <input type="password" name="confirmpassword" id="confirmpassword" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name" />
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
+                    </form>
+                </div>
+                {{-- delete account --}}
+                <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <form class="space-y-6" action="#">
+                        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Delete Account</h5>
+                        <p class="text-sm text-gray-500 max-w-lg">
+                            Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.
+                        </p>
+                        <button type="submit" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Delete</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
-@livewireScripts
-
+        @include('layouts.footer')
+    </body>
+</html>
