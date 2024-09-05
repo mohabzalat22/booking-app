9e96e997187f943a6cbc891733184f9ce3a9ddeb
mohabzalat <mohabzalat20@gmail.com>
2024-09-05 08:22:02 +0300

    adding tickets to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,20 @@
         @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body class="font-sans bg-gray-100 antialiased dark:bg-black dark:text-white/50 max-w-[1500px] mx-auto">
+        
+        <!-- Main modal -->
+        <div id="qr-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div class="relative p-4 w-full max-w-2xl max-h-full">
+                <!-- Modal content -->
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <!-- Modal body -->
+                    <div class="p-4 md:p-5 space-y-4">
+                        <img class="max-w-md max-h-md mx-auto" src="http://127.0.0.1:8000/images/1.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+  
         {{-- dashboard --}}
         @include('layouts.custom-navigation')
         <div class="container mx-auto">
@@ -83,6 +97,36 @@
                         <button type="submit" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Delete</button>
                     </form>
                 </div>
+            </div>
+            {{-- tikets --}}
+            <div class="p-10" data-modal-target="qr-modal" data-modal-toggle="qr-modal">
+                <a class="mx-auto hover:scale-110 ease-in-out flex flex-wrap items-center w-full max-w-sm duration-100 m-2 bg-white border border-gray-200 rounded-lg shadow">
+                    <div class="relative"> 
+                        <img class="hidden lg:block rounded-t-lg w-full" src="http://127.0.0.1:8000/images/2.png" alt="">
+                        <div class="absolute top-2 right-2 rounded-full w-10 h-10 bg-slate-800 text-white p-2 font-semibold flex justify-center items-center">X5</div>
+                    </div>
+                    <div class="flex flex-wrap justify-between w-full">
+                    <div class="p-5">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">minus assumenda</h5>
+                        <div class="flex items-center my-2">
+                            <svg class="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z"></path>
+                            </svg>                              
+                            <p class="ms-4 text-sm text-gray-500 font-bold">30 September onwards | 1978</p>
+                        </div>
+                        <div class="flex items-center my-2">
+                            <svg class="w-6 h-6 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"></path>
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"></path>
+                            </svg>                                                          
+                            <p class="ms-4 text-sm text-gray-500 font-bold capitalize">Fadelchester facere South Dakota</p>
+                        </div>
+                    </div>
+                    <div class="border-s flex items-center justify-between ms-auto">
+                        <p class="font-bold text-dark-800 m-2 p-0 text-2xl">200$</p>
+                    </div>
+                    </div>
+                </a>
             </div>
         </div>
         @include('layouts.footer')
