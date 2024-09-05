abe0d42c1128a53614f54aeb06d819d62025a590
mohabzalat <mohabzalat20@gmail.com>
2024-09-05 22:23:41 +0300

    adding error layer for personal info and passes.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -45,11 +45,20 @@
                     <li class="text-xl hover:bg-slate-300 p-5">Wallets</li>
                 </ul>
             </div>
+            @if ($errors->any())
+                <div class="text-xl text-red-600">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             {{-- profile --}}
             <div class="p-10">
                 {{-- profile information --}}
                 <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <form x-on:submit.prevent class="space-y-6" action="http://127.0.0.1:8000/profile" method="POST">
+                    <form x-on:submit.prevent class="space-y-6" action="{{route('profile.update')}}" method="POST">
                         @method('patch')
                         @csrf
                         <h5 class="text-xl font-medium text-gray-900 dark:text-white">Profile Information</h5>
@@ -59,19 +68,25 @@
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
                             <input type="text" name="name" id="name" value="{{Auth::user()->name}}" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name" />
+                            @error('name')
+                                <div class="text-sm text-red-500">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                             <input type="email" name="email" id="email" value="{{Auth::user()->email}}"  class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" />
+                            @error('email')
+                                <div class="text-sm text-red-500">{{ $message }}</div>
+                            @enderror
                         </div>
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
                     </form>
                 </div>
                 {{-- password --}}
                 <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <form class="space-y-6" action="http://127.0.0.1:8000/password" method="post">
-                        @method('PUT')
+                    <form class="space-y-6" action="{{ route('password.update') }}" method="post">
                         @csrf
+                        @method('put')
                         <h5 class="text-xl font-medium text-gray-900 dark:text-white">Update Password</h5>
                         <p class="text-sm text-gray-500">
                             Ensure your account is using a long, random password to stay secure.
@@ -79,21 +94,32 @@
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Password</label>
                             <input type="password" name="current_password" id="password" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="current password" />
+                            @if($errors->updatePassword->has('current_password'))
+                                <div class="text-sm text-red-500">{{ $errors->updatePassword->first('current_password') }}</div>
+                            @endif
                         </div>
                         <div>
                             <label for="newpassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
                             <input type="password" name="password" id="newpassword" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="new password" />
+                            @if($errors->updatePassword->has('password'))
+                                <div class="text-sm text-red-500">{{ $errors->updatePassword->first('password') }}</div>
+                            @endif
                         </div>
                         <div>
-                            <label for="confirmpassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                            <input type="password" name="confirmpassword" id="confirmpassword" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="confirm password" />
+                            <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                            <input type="password" name="password_confirmation" id="password_confirmation" class="max-w-lg bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="confirm password" />
+                            @if($errors->updatePassword->has('password_confirmation'))
+                                <div class="text-sm text-red-500">{{ $errors->updatePassword->first('password_confirmation') }}</div>
+                            @endif
                         </div>
                         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
                     </form>
                 </div>
                 {{-- delete account --}}
                 <div class="w-full my-4 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <form class="space-y-6" action="#">
+                    <form class="space-y-6" action="{{route('profile.destroy')}}">
+                        @csrf
+                        @method('delete')
                         <h5 class="text-xl font-medium text-gray-900 dark:text-white">Delete Account</h5>
                         <p class="text-sm text-gray-500 max-w-lg">
                             Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.
