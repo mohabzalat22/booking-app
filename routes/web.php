93de6a79a8599fba841d4dfd6379106d513de9fc
mohabzalat <mohabzalat20@gmail.com>
2024-09-08 20:08:03 +0300

    updating hrefs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
+
 use Illuminate\Support\Facades\Route;
 use App\Models\Ticket;
 
@@ -27,3 +29,5 @@ Route::get('/', function () {
 Route::get('/tickets', function () {
     return view('tickets');
 })->name('tickets');
+
+Route::get('/ticket/{id}', [TicketController::class, 'index'])->name('ticket.index');
