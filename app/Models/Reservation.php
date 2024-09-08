811f9af925501c470d57396866fffdf76417ba45
mohabzalat <mohabzalat20@gmail.com>
2024-09-08 20:04:42 +0300

    making resevation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'ticket_id',
+        'name',
+        'serial',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function ticket(): HasOne
+    {
+        return $this->hasOne(Ticket::class);
+    }
+}
