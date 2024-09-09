b46209993d6b3c9067471fc00b9223b6bc25bb28
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 19:54:03 +0300

    ading venue factory

---
+++ database/factories/VenueFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Venue>
+ */
+class VenueFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ticket_id' => Ticket::factory(),
+            'country' => fake()->state(),
+            'city' => fake()->city(),
+            'place' => fake()->word(),
+            'location' => fake()->address(),
+        ];
+    }
+}
