43e81788dc485eec437ccfad5a569bbd75b5c928
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 19:52:11 +0300

    adding quantitiy model v2.0

---
+++ app/Models/Quantity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Quantity extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'type_id',
+        'quantity',
+    ];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
