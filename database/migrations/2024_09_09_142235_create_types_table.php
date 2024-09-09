a30d852a53ad2bc049b31c2d73e244c411e4a2fb
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 18:14:03 +0300

    new types table schema v2.0

---
+++ database/migrations/2024_09_09_142235_create_types_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ticket_id');
+            $table->foreign('ticket_id')->refrences('id')->on('tickets')->onDelete('cascade');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('types');
+    }
+};
