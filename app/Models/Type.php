5fa1c2cae4aee64904839d870d2375878d226075
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 18:50:51 +0300

    adding relations to model

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'ticket_id',
+        'type',
+    ];
+
+    public function ticket(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function quantity(): HasOne
+    {
+        return $this->hasOne(Quantity::class);
+    }
+
+    public function price(): HasOne
+    {
+        return $this->hasOne(Price::class);
+    }
 }
