3ec0441471de3cc6ae10a2b3297172b1c76b3425
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 18:50:02 +0300

    adding relations to model

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Price extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type_id',
+        'price',
+        'discount',
+        'tax',
+    ];
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
