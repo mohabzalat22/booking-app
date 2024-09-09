c8297c848ff418e0feeab9179180fe4316cbe95c
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 18:50:19 +0300

    adding relations to model

--- app/Models/Qunatity.php
+++ app/Models/Qunatity.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Qunatity extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'type_id',
+        'quantity',
+    ];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
