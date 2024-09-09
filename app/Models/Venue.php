32f02700907e7c05109009a7d6624b52eeeb51d1
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 18:51:12 +0300

    adding relations to model

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Venue extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'ticket_id',
+        'country',
+        'city',
+        'place',
+        'location',
+    ];
+    public function ticket(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class);
+    }
 }
