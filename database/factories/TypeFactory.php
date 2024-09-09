e8266c6143ea0879072095da697bdcb3bd0a5e3c
mohabzalat <mohabzalat20@gmail.com>
2024-09-09 19:53:36 +0300

    ading type factory

---
+++ database/factories/TypeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Ticket;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Type>
+ */
+class TypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ticket_id' => Ticket::factory(),
+            'type' => fake()->word(),
+        ];
+    }
+}
