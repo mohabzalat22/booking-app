507eaff2f5e9a7983d7034328654c0563cfbe98d
mohabzalat <mohabzalat20@gmail.com>
2024-09-10 16:28:46 +0300

    adding types to controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -9,7 +9,8 @@ class TicketController extends Controller
 {
     public function index(Request $request){
         return view('ticketDetails', [
-            'ticket' => Ticket::find($request->id)
+            'ticket' => Ticket::where('id', $request->id)->with(['venue','types'])->first(),
+            'types' => Ticket::with('types.price')->find($request->id)->types()->get()
         ]);
     }
 }
