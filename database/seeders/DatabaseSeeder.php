4f19d4b31a9322a9921957af26325bf112366c9e
mohabzalat <mohabzalat20@gmail.com>
2024-09-10 16:31:25 +0300

    increase number of records in factory database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Ticket;
+use App\Models\Venue;
+use App\Models\Type;
+use App\Models\Quantity;
+use App\Models\Price;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,7 +25,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Ticket::factory(20)->create();
+        Ticket::factory(10)
+        ->has(Venue::factory())
+        ->has(
+            Type::factory(2)
+            ->has(Quantity::factory())
+            ->has(Price::factory())
+            )
+        ->create();
 
     }
 }
