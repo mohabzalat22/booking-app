15fe51e8136d5cc33d32ba210ed37d63da0c5a65
mohabzalat <mohabzalat20@gmail.com>
2024-09-11 15:28:44 +0300

    toast

---
+++ app/Livewire/ToastDangerComponent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ToastDangerComponent extends Component
+{
+    public $message;
+    public function render()
+    {
+        return view('livewire.toast-danger-component');
+    }
+
+    public function mount($message)
+    {
+        $this->message = $message;
+    }
+}
