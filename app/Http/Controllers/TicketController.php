b78768ab7f9b741612a3f881386445cacc8055e5
mohabzalat <mohabzalat20@gmail.com>
2024-09-11 15:33:06 +0300

    making controller just return blade file

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Ticket;
-
 class TicketController extends Controller
 {
-    public function index(Request $request){
-        return view('ticketDetails', [
-            'ticket' => Ticket::where('id', $request->id)->with(['venue','types'])->first(),
-            'types' => Ticket::with('types.price')->find($request->id)->types()->get()
-        ]);
+    public function index(){
+        return view('ticketDetails');
     }
 }
